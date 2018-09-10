f143382677485e5fe293f9d8741ab2eabe438925
zhaorongkui <563374704@qq.com>
2018-09-10 08:37:01 +0800

    登录与注册静态页面
    登录与注册静态页面

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule
