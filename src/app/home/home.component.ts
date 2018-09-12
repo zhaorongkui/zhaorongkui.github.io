cea75dc1f4af5bdca9d519c86b2496f837662ddf
hanyanfei <hanyannanfei@163.com>
2018-09-12 17:45:23 +0800

    测试共享修改

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css','../../res/layui/css/layui.css','../../res/css/global.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
