6b1264925187e53508c80e2b45e7edf2e9e77244
zhaorongkui <563374704@qq.com>
2018-09-12 18:45:50 +0800

    home 路由格式调整

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from './/app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
+    HomeComponent,
     RegisterComponent
   ],
   imports: [
